f75c807a439f7c27d81af39b6fe8f97a307c389b
DispicableLee <lee.robert053@gmail.com>
2022-09-22 13:19:43 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", (e) => {
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (e)=>{
+
+  e.preventDefault();
+  
+    const newUl = document.getElementById("tasks")
+    const newLi = document.createElement("li")
+    const removeButton = document.createElement("button")
+  console.log("blah")
+
+    newLi.innerText = e.target["new-task-description"].value
+    newUl.append(newLi);
+    newLi.append(removeButton)
+    removeButton.addEventListener("click", ()=>{
+      newLi.remove();
+    })
+  }
+  
+)
+}
+)
